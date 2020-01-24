77bc3c247a2d056896d0dce2d8f44f236bc5ec1b
jeonghoheo <heojeongho1992@gmail.com>
2020-01-24 20:09:47 +0900

    의존성 주입을 위한 테스트해주기

---
+++ spec/DI/DiContainer__00.js
@@ -0,0 +1,5 @@
+const DiContainer = function() {};
+
+DiContainer.prototype.register = function(name, dependencies, func) {};
+
+export default DiContainer;
---
+++ spec/DI/Dicontainer__01.js
@@ -0,0 +1,37 @@
+const DiContainer = function() {
+  // 반드시 생성자로 객체를 생성하게 한다.
+  if (!(this instanceof DiContainer)) {
+    return new DiContainer();
+  }
+};
+
+DiContainer.prototype.messages = {
+  registerRequiresArgs:
+    "이 생성자 함수는 인자가 3개 있어야 합니다: " + "문자열, 문자열배열, 함수."
+};
+
+DiContainer.prototype.register = function(name, dependencies, func) {
+  let ix;
+
+  if (
+    typeof name !== "string" ||
+    !Array.isArray(dependencies) ||
+    typeof func !== "function"
+  ) {
+    console.log(`[Jeongho][Line - 21][File - Dicontainer__01.js] 
+    ${this.messages.registerRequiresArgs}
+    `);
+    throw new Error(this.messages.registerRequiresArgs);
+  }
+
+  for (ix = 0; ix < dependencies.length; ++ix) {
+    if (typeof dependencies[ix] !== "string") {
+      console.log(`[Jeongho][Line - 29][File - Dicontainer__01.js] 
+        ${this.messages.registerRequiresArgs}
+        `);
+      throw new Error(this.messages.registerRequiresArgs);
+    }
+  }
+};
+
+export default DiContainer;
